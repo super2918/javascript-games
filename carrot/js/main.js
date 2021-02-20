6dca5b9c8ae116ba48ff195b07c77b23605c04d6
mc2918 <tlsdud1375@gmail.com>
2021-02-21 00:26:43 +0900

    Add gametimer

--- carrot/js/main.js
+++ carrot/js/main.js
@@ -2,13 +2,77 @@
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
+const gameButton = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+let started = false;
+let socre = 0;
+let timer = undefined;
+
+gameButton.addEventListener('click', () => {
+  if(started) { // 게임이 시작되었다면 중지
+    stopGame();
+  } else { // 게임이 시작하지 않았다면 시작
+    startGame();
+  }
+  started = !started; 
+  /*
+  게임이 진행중이라면(started 는 true로), 게임을 중지하는 함수를 호출하고,
+  started의 반대값인 false를 started에 할당해 줘서, 다음에 버튼이 클릭 된다면 started는 false가 되므로 시작하는 함수를 호출한다.
+  */
+});
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameButton.querySelector('.fa-play');
+  icon.classList.add('fa-stop');
+  icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = 'visible';
+  gameScore.style.visibility = 'visible';
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+
+  timer = setInterval(() => {
+    if(remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return
+    } 
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(timer) {
+  console.log(timer);
+  const minutes = Math.floor(timer/ 60);
+  const seconds = timer % 60;
+
+  gameTimer.textContent = `${minutes} : ${seconds}`;
+}
 
 function initGame() {
-  // 벌레와 당근을 생성한 뒤 field에 추가해준다.
-  console.log(fieldRect);
+  // reset
+  field.innerHTML = '';
+  // score
+  gameScore.textContent = CARROT_COUNT;
+
   addItem('carrot', CARROT_COUNT, './img/carrot.png');
   console.log(addItem)
   addItem('bug', BUG_COUNT, './img/bug.png')
@@ -41,5 +105,3 @@ function addItem(className, count, imgPath) {
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
-initGame(); 
