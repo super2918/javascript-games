ca7eb4354ac54d0db6edbe1990a31423f529bcd1
mc2918 <tlsdud1375@gmail.com>
2021-02-20 10:37:00 +0900

    Fix carrot, bug count to constant

--- carrot/js/main.js
+++ carrot/js/main.js
@@ -1,14 +1,17 @@
 'use strict';
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
 
 function initGame() {
   // 벌레와 당근을 생성한 뒤 field에 추가해준다.
   console.log(fieldRect);
-  addItem('carrot', 5, './img/carrot.png');
+  addItem('carrot', CARROT_COUNT, './img/carrot.png');
   console.log(addItem)
-  addItem('bug', 5, './img/bug.png')
+  addItem('bug', BUG_COUNT, './img/bug.png')
 }
 
 function addItem(className, count, imgPath) {
